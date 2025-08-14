962c0b6795fedc117e86f15f37601865288066cf
ratHippo <91836375+ratHippo@users.noreply.github.com>
2025-08-14 12:59:31 +0200

    Update gen.sh

--- gen/gen.sh
+++ gen/gen.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
-rm site/*.html
-for file in `ls src/*.md`
-do
-    echo $file
-    file=${file#src/}
-    file=${file%.md}
-    echo $file
-    pandoc \
-        -s \
-        --template gen/template.html \
-        -i src/$file.md \
-        -o site/$file.html
+cd "${BASH_SOURCE%gen.sh}/.."
+
+rsync -a --exclude '*.md' src/ site/
+
+for file in `find src -type f`; do
+    if [[ $file == *.md ]]; then
+        file=${file#src/}
+        file=${file%.md}
+        echo "converting $file"
+        pandoc \
+            -s \
+            --template gen/template.html \
+            -i src/$file.md \
+            -o site/$file.html \
+            --mathml
+    fi
 done
